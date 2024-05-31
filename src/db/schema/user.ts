43f189f88da993a5cdfed870a9131e5f1942aabe
Fractal-Tess <vgfractal@gmail.com>
2024-05-31 05:11:43 +0300

    fix: correct typo

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -3,6 +3,6 @@ import { int, mysqlTable, varchar } from 'drizzle-orm/mysql-core';
 export const user = mysqlTable('user', {
   id: int('id').primaryKey().autoincrement(),
   username: varchar('username', { length: 64 }).notNull(),
-  eamil: varchar('email', { length: 320 }).notNull(),
+  email: varchar('email', { length: 320 }).notNull(),
   passwordHash: varchar('passwordHash', { length: 255 }).notNull(),
 });
