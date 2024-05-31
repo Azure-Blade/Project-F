832b9b36ca8c31a6543bf70820a16dea9f18c781
Fractal-Tess <vgfractal@gmail.com>
2024-05-31 04:58:37 +0300

    chore: refactor page

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,21 +1,26 @@
-'use server';
 import { db, schema } from '@/db';
 import { faker } from '@faker-js/faker';
 import * as argon2 from 'argon2';
+import { InferInsertModel } from 'drizzle-orm';
+
+export const dynamic = 'force-dynamic';
 
 export default async function page() {
   const pass = faker.internet.password();
   const passwordHash = await argon2.hash(pass);
-  const result = await db
-    .insert(schema.user)
-    .values([
-      {
-        id: Math.random() * 1000,
-        username: faker.internet.userName(),
-        eamil: faker.internet.email(),
-        passwordHash,
-      },
-    ]);
-  console.log(result[0].insertId);
-  return <h1>Hello from user page</h1>;
+  const user = {
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    passwordHash,
+  } satisfies InferInsertModel<typeof schema.user>;
+  const result = await db.insert(schema.user).values(user);
+
+  return (
+    <>
+      <h1>
+        A user with the name of {user.username} was inserted with an id of{' '}
+        {result[0].insertId}
+      </h1>
+    </>
+  );
 }
